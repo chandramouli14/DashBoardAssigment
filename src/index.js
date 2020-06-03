d9498dd6a2e69cc683912012a480693385da6447
chandramouli.kanta <chandramouli.kanta@ggktech.com>
2020-06-03 18:13:25 +0530

    added all requirements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-
 let taskList;
+let url = "http://localhost:4000/tasks/";
 
-let url = "http://localhost:3000/tasks/"
+let selectLabel = document.getElementById("selectall");
+let labels = document.getElementsByClassName("labelSelected")
 
+function selectAll() {
+    Object.values(labels).map(label => {
+        label.checked = selectLabel.checked;
+        ShowLabels(label)
+    })
+}
+
+function ShowLabels(label) {
+    let section = document.getElementById(label.name)
+    if (label.checked) {
+        section.className = "col-lg bg-light mx-2";
+        if (Object.values(labels).filter(label => label.checked).length == 5) {
+            selectLabel.checked = true;
+        }
+    }
+    else {
+        section.className = "hide"
+        selectLabel.checked = false
+    }
+}
 
 function intialize() {
     getTasks(url).then(res => res.json().then(tasks => {
         taskList = tasks;
-        taskList.map(task => CreateTask(task))
+        taskList.map(task => CreateTask(task));
+        ChartView();
+        selectAll()
     }))
 }
 
@@ -42,28 +65,135 @@ function storeTask(url, task) {
     })
 }
 
+function ChartView() {
+    var doTasks = [];
+    var doingTasks = [];
+    var doneTasks = [];
+    var devTasks = [];
+    var qaTasks = [];
+    if (taskList.length > 0) {
+        taskList.forEach(element => {
+            if (element.status == "toDo") {
+                doTasks.push(element)
+            }
+            else if (element.status == "done") {
+                doneTasks.push(element)
+            }
+            else if (element.status == "inProgress") {
+                doingTasks.push(element)
+            }
+            else if (element.status == "inQA") {
+                qaTasks.push(element)
+            }
+            else if (element.status == "devDone") {
+                devTasks.push(element)
+            }
+        })
+    }
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true, theme: "light2",
+        title: {
+            text: " Chart View Of Tasks:",
+        },
+        data: [{
+            type: "line",
+            startAngle: 90,
+            indexLabelFontSize: 18,
+            radius: 500,
+            yValueFormatString: "##0.00\"%\"",
+            indexLabel: "{label} {y}",
+            click: explodePie,
+            dataPoints: [
+                { y: GetTaskPercentage("toDo"), label: "TODO" },
+                { y: GetTaskPercentage("inProgress"), label: "INPROGRESS" },
+                { y: GetTaskPercentage("done"), label: "DONE" },
+                { y: GetTaskPercentage("devDone"), label: "DEV DONE" },
+                { y: GetTaskPercentage("inQA"), label: "IN QA" }
+            ]
+        }]
+    });
+    chart.render();
+
+    function GetTaskPercentage(type) {
+        if (type == "toDo") {
+            return doTasks.length > 0 ?
+                100 * doTasks.length / taskList.length :
+                0;
+        }
+        else if (type == "done") {
+            return doneTasks.length > 0 ?
+                100 * doneTasks.length / taskList.length :
+                0;
+        }
+        else if (type == "inProgress") {
+            return doingTasks.length > 0 ?
+                100 * doingTasks.length / taskList.length :
+                0;
+        }
+        else if (type == "inQA") {
+            return qaTasks.length > 0 ?
+                100 * qaTasks.length / taskList.length :
+                0;
+        }
+        else if (type == "devDone") {
+            return devTasks.length > 0 ?
+                100 * devTasks.length / taskList.length :
+                0;
+        }
+    }
+
+    function explodePie(e) {
+        for (var i = 0; i < e.dataSeries.dataPoints.length; i++) {
+            if (i !== e.dataPointIndex)
+                e.dataSeries.dataPoints[i].exploded = false;
+        }
+    }
+}
+
+function showView(view) {
+    hideView();
+    document.getElementById(view).className = ""
+    if (view == "chartView") {
+        ChartView();
+    }
+}
+const chartView = document.getElementById("chartView");
+const dashBoard = document.getElementById("dashBoard");
+const addTask = document.getElementById("addTask");
+const settings = document.getElementById("settings")
+function hideView() {
+    chartView.className = dashBoard.className = addTask.className = settings.className = "hide"
+}
+
 function CreateTask(task) {
     var node = GetTaskType(task.status);
     if (node) {
-        var wrapper = document.createElement("div");
-        var card = document.createElement("div");
-        var body = document.createElement("div");
-        var title = document.createElement("p");
+        let wrapper = document.createElement("div");
+        let card = document.createElement("div");
+        let body = document.createElement("div");
+        let title = document.createElement("p");
         title.setAttribute("class", "card-title");
+        title.className = "text-primary"
         title.textContent = "Task-" + task.id
-        var icon = document.createElement("i");
-
-        icon.className = "fa fa-user-circle"
+        let icon = document.createElement("i");
+        icon.className = "fa fa-user-circle text-primary"
         icon.style.float = "right";
         title.appendChild(icon)
-        var taskName = document.createElement("p");
+        let taskName = document.createElement("p");
         taskName.textContent = task.name
-        var subTask = document.createElement("p");
-        subTask.textContent = task.content
-        var check = document.createElement("i")
-        check.className = "fa fa-check-square"
-        var equals = document.createElement("i")
-        equals.className = "fa fa-equals"
+        taskName.className = "text-warning"
+        let assignee = document.createElement("p");
+        assignee.textContent = "Assign to:" + task.assignee;
+        assignee.className = "text-secondary"
+        let priority = document.createElement("span");
+        priority.textContent = task.priority
+        priority.className = "bg-success text-white"
+        let icons = document.createElement("p");
+        let check = document.createElement("i")
+        check.className = "fa fa-check-square text-primary"
+        let equals = document.createElement("i")
+        equals.className = "fa fa-equals  text-warning"
+        icons.append(check, equals);
         body.setAttribute("class", "card-body");
         card.setAttribute("class", "card my-8");
         card.style.width = "100%";
@@ -72,7 +202,7 @@ function CreateTask(task) {
         wrapper.setAttribute("ondragstart", "drag(event)");
         wrapper.id = task.id;
         wrapper.style.cursor = "grab"
-        body.append(title, taskName, subTask, check, equals);
+        body.append(title, taskName, assignee, priority, icons);
         card.appendChild(body);
         wrapper.appendChild(card);
         node.appendChild(wrapper);
@@ -85,6 +215,10 @@ function GetTaskType(value) {
             return document.getElementById("toDo");
         case 'inProgress':
             return document.getElementById("inProgress");
+        case 'devDone':
+            return document.getElementById("devDone");
+        case 'inQA':
+            return document.getElementById("inQA");
         case 'done':
             return document.getElementById("done");
     }
@@ -99,10 +233,11 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
+
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    if (ev.target.id == "toDo" || ev.target.id == "inProgress" || ev.target.id == "done") {
+    if (ev.target.id == "toDo" || ev.target.id == "inProgress" || ev.target.id == "done" || ev.target.id == "devDone" || ev.target.id == "inQA") {
         ev.target.appendChild(document.getElementById(data));
         changeStatus(data, ev.target.id)
     }
@@ -123,17 +258,25 @@ function changeStatus(id, type) {
 function addNewTask() {
     const taskName = document.getElementById("name")
     const status = document.getElementById("status")
-    const content = document.getElementById("content");
-    let task = {};
-    task.status = status.value;
-    task.name = taskName.value;
-    task.content = content.value;
-    if (status.value == "none") {
-        alert("Select Task Status");
+    const assignee = document.getElementById("assignee");
+    const priority = document.getElementById("priority");
+
+    if (status.value == "none" || priority.value == "none") {
+        alert("Alert Please fill all required fields.");
         return;
     }
-    storeTask(url, task)
-        .then((res) => { debugger; window.location.href = "index.html" })
+    else {
+        let task = {};
+        task.status = status.value;
+        task.name = taskName.value;
+        task.assignee = assignee.value;
+        task.priority = priority.value;
+        storeTask(url, task)
+            .then((res) => {
+                alert("Task Added SucessFully");
+                window.location.href = "index.html"
+            })
+    }
 }
 
 window.onload = intialize
